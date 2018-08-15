6bbc1bde237aa81b7b16972fa6fd133eef560a44
Frank <fgunsch.webdev@gmail.com>
2018-08-15 18:58:58 -0400

    added timing feature

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 const BogoLoops = function(){
+    let milestoneData = {
+    One:"You've gone 1 loop.",
+    Ten:"You've gone 10 loops",
+    Hundred:"You've gone 100 loops",
+    Thousand:"You've gone 1000 loops"
+    };
+    console.log(milestoneData);
     let startTime = (new Date()).getTime();
-    let highPower  = 10;
+    let exponentLimit = 8;
+    let baseTen = 10;
 //goes up to 10 Billion itterations...
-for(power=0;power<=highPower;power++){
-    for(i=0;i<(Math.pow(highPower, power));i++){}
+for(power=0;power<=exponentLimit;power++){
+    for(i=0;i<(Math.pow(baseTen, power));i++){}
     //Screen display bottleneck.
     let sinceTimeStart = (new Date()).getTime() - startTime;
+    $( ".bogoDescription" ).append( `<p> ${sinceTimeStart} milliseconds passed from doing nothing 10^${power} times. </p>` );
     console.log(sinceTimeStart +'ms passed.');
     console.log('Looped through 10^'+ power +' times');
-    console.log('Going up a power...');
     }
-console.log('Finished. Last iteration looped 10 Billion times');
-console.log('next power would of been:' + power);
+    $(".bogoGoButtonContainer").remove();
+console.log(`Finished. Last iteration looped + ${Math.pow((baseTen),exponentLimit)}`);
+console.log(`next power would of been: + ${Math.pow(baseTen,exponentLimit+1)}`);
 };
 
 $(".bogoGoButton").click(BogoLoops);

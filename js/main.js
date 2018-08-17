e906b466f3def3992c9e6147de32be6ddcd5f6ee
Frank <fgunsch.webdev@gmail.com>
2018-08-17 01:28:06 -0400

    just more formatting.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
-function appendTimeStamp(sinceTimeStart,power){
-    $( ".bogoDescription" ).append( `<p> ${sinceTimeStart} milliseconds passed from doing nothing 10^${power} times. </p>` );
+function appendTimeStamp(sinceTimeStart, increment) {
+  $(".bogoDescription").append(`😎 ${sinceTimeStart} ms passed from doing nothing ${1000000*increment} times. 😎`);
 };
 
-const BogoLoops = function(e){
-    e.preventDefault();
-    let milestoneData = {
-    One:"You've gone 1 loop.",
-    Ten:"You've gone 10 loops",
-    Hundred:"You've gone 100 loops",
-    Thousand:"You've gone 1000 loops"
-    };
-    console.log(milestoneData);
-    let startTime = (new Date()).getTime();
-    let exponentLimit = 8;
-    let baseTen = 10;
-//goes up to 10 Billion itterations...
-for(power=0;power<=exponentLimit;power++){
-    for(i=0;i<(Math.pow(baseTen, power));i++){}
+var incrementLimit = 0;
+const BogoLoops = function (e) {
+  incrementLimit += 1;
+  e.preventDefault();
+
+  let startTime = (new Date()).getTime();
+
+  for (increment = (incrementLimit - 1); increment < incrementLimit; increment++) {
+    for (i = 0; i < (increment * 1000000); i++) {}
     //Screen display bottleneck.
     let sinceTimeStart = (new Date()).getTime() - startTime;
-    appendTimeStamp(sinceTimeStart,power); 
-    console.log(sinceTimeStart +'ms passed.');
-    console.log('Looped through 10^'+ power +' times');
-    }
-    //$(".bogoGoButtonContainer").remove();
-console.log(`Finished. Last iteration looped + ${Math.pow((baseTen),exponentLimit)}`);
-console.log(`next power would of been: + ${Math.pow(baseTen,exponentLimit+1)}`);
+    appendTimeStamp(sinceTimeStart, increment);
+    console.log(sinceTimeStart + 'ms passed.');
+  }
 };
 
-const changeButton = function() {
-    document.getElementById("mainButton").innerHTML = "Do More Nothing";
-}
+const changeButton = function () {
+  document.getElementById("mainButton").innerHTML = "Do More Nothing";
+};
 $(".bogoGoButton").click(changeButton);
 $(".bogoGoButton").click(BogoLoops);
+
+//Remember to do more proper code structure when you make longer stuff.

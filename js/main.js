728f8d60c7afd1e8b62ee86007b87a8766780bc9
Frank <fgunsch.webdev@gmail.com>
2018-08-17 19:16:06 -0400

    cODE ENCAPSULATION

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+(function(){
+var numberOfLoops = 1000000;
+var incrementLimit = 0;
+
 function appendTimeStamp(sinceTimeStart, increment) {
   $(".bogoDescription").append(`😎 ${sinceTimeStart} ms passed from doing nothing ${1000000*increment} times. 😎`);
 };
 
-var incrementLimit = 0;
 const BogoLoops = function (e) {
   incrementLimit += 1;
   e.preventDefault();
@@ -10,7 +13,7 @@ const BogoLoops = function (e) {
   let startTime = (new Date()).getTime();
 
   for (increment = (incrementLimit - 1); increment < incrementLimit; increment++) {
-    for (i = 0; i < (increment * 1000000); i++) {}
+    for (i = 0; i < (increment * numberOfLoops); i++) {}
     //Screen display bottleneck.
     let sinceTimeStart = (new Date()).getTime() - startTime;
     appendTimeStamp(sinceTimeStart, increment);
@@ -23,5 +26,5 @@ const changeButton = function () {
 };
 $(".bogoGoButton").click(changeButton);
 $(".bogoGoButton").click(BogoLoops);
-
+}());
 //Remember to do more proper code structure when you make longer stuff.

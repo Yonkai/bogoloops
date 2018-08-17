d7936639cb553928e31eeaef82f049c0cc74a626
Frank <fgunsch.webdev@gmail.com>
2018-08-16 20:00:04 -0400

    more code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function appendTimeStamp(sinceTimeStart,power){
+    $( ".bogoDescription" ).append( `<p> ${sinceTimeStart} milliseconds passed from doing nothing 10^${power} times. </p>` );
+};
+
 const BogoLoops = function(){
     let milestoneData = {
     One:"You've gone 1 loop.",
@@ -14,13 +18,19 @@ for(power=0;power<=exponentLimit;power++){
     for(i=0;i<(Math.pow(baseTen, power));i++){}
     //Screen display bottleneck.
     let sinceTimeStart = (new Date()).getTime() - startTime;
-    $( ".bogoDescription" ).append( `<p> ${sinceTimeStart} milliseconds passed from doing nothing 10^${power} times. </p>` );
+    appendTimeStamp(sinceTimeStart,power); 
     console.log(sinceTimeStart +'ms passed.');
     console.log('Looped through 10^'+ power +' times');
     }
-    $(".bogoGoButtonContainer").remove();
+    //$(".bogoGoButtonContainer").remove();
 console.log(`Finished. Last iteration looped + ${Math.pow((baseTen),exponentLimit)}`);
 console.log(`next power would of been: + ${Math.pow(baseTen,exponentLimit+1)}`);
 };
 
+const changeButton = function() {
+    document.getElementById("mainButton").innerHTML = "Do More Nothing";
+    $("a").removeClass("bogoGoButton");
+}
+
+$(".bogoGoButton").click(changeButton);
 $(".bogoGoButton").click(BogoLoops);
